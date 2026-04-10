49b484b02405f9d95ca4bb06fdcdb031dbc1b6e1
s0m1k111 <spavel1207@gmail.com>
2026-04-10 09:14:14 +0300

    Step 7: returning JSON from routes

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -6,4 +8,27 @@ app.MapGet("/about", () => "это мой превый asp.Net core сервер
 app.MapGet("/time", () => $"время на сервере {DateTime.Now}");
 app.MapGet("/hello/{name}", (string name) => $"привет , {name}");
 app.MapGet("/sum/{a}/{b}", (int a, int b) => $"{a + b}");
+app.MapGet("student", () => new
+{
+    Name = "Динара Жанатпаева",
+    Group = "исп-232",
+    Year = 18,
+    IsActive = true
+
+});
+app.MapGet("/subjects", () => new[]
+{
+    "РПМ",
+"РМП" ,
+"ИСРПО" ,
+"СП" ,
+});
+app.MapGet("/product/{id}", (int id) => new Product(
+Id: id,
+Name: $"товар #{id}",
+Price: id * 99.99m,
+InStock: id % 2 == 0
+
+));
 app.Run();
+record Product(int Id, string Name, decimal Price, bool InStock);
